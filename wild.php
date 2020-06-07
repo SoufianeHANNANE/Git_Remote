50dcb1c462a85251e2afa89d0b5a5b2f3465ca72
SoufianeHANNANE <Soufiane.hannane.pro@gmail.com>
2020-06-07 17:02:40 +0200

    A la wild schol on code en chausette

--- wild.php
+++ wild.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE php> 
+<html> 
+  <head> 
+    <meta charset="UTF-8"> 
+    <title>Projet git remote</title> 
+  </head> 
+  <body> 
+  <h1> 
+  <p> A la wild code school on code en chaussettes<p>
+  <h1>
+   
+  </body> 
+</html>
